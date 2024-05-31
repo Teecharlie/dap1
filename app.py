c48ee158c880d8ab1de72aed12b2fc12a4592984
Teecharlie <okaidza2722@gmail.com>
2024-05-31 18:53:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 
 def formatIndex(df):
-    df['S/No.'] = range(1, len(df) + 1)
+    df['S/No.'] = range(1, len(df) + 1) #so this means, the serial number change the index, start if from 1 and run it for the length of the list and increase the number by 1
     df = df.set_index('S/No.')
     return df
 
@@ -16,3 +16,6 @@ total_sales_products=df[['Product Name','Sales']].groupby('Product Name').agg({'
 total_sales_products['Sales']=round(total_sales_products['Sales'],2)
 total_sales_products=formatIndex(total_sales_products)
 st.dataframe(total_sales_products.head(10))
+
+fig=px.bar(total_sales_products,x='Product Name',y='Sales')
+st.plotly_chart(fig,use_container_width=True)
